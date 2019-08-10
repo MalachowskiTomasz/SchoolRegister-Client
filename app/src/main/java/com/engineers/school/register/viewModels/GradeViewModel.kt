0722de2e11bf58e37d3f08ebb7ac7f65a97cb816
Tomasz Małachowski <malachowski.tomasz@outlook.com>
2019-08-10 13:35:52 +0200

    Created common Menu Activity

---
+++ app/src/main/java/com/engineers/school/register/viewModels/GradeViewModel.kt
@@ -0,0 +1,8 @@
+package com.engineers.school.register.viewModels
+
+import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.SavedStateHandle
+
+class GradeViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+    val
+}
