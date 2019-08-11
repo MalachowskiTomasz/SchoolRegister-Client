198e68f66d2d18ca27861f69c0bcb8b95cdcb108
Tomasz Małachowski <malachowski.tomasz@outlook.com>
2019-08-11 22:12:24 +0200

    Added cache to course summary repository

--- app/src/main/java/com/engineers/school/register/repositories/CourseSummaryRepository.kt
+++ app/src/main/java/com/engineers/school/register/repositories/CourseSummaryRepository.kt
@@ -1,27 +1,48 @@
 package com.engineers.school.register.repositories
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.engineers.school.register.entities.CourseSummary
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class CourseSummaryRepository(private val courseRemoteRepository: CourseSummaryRemoteRepository) {
 
+    private val executorService = Executors.newSingleThreadScheduledExecutor()
+
+    private var studentId: Long? = null
+    private val dataCache = MutableLiveData<List<CourseSummary>>()
+
+    init {
+        executorService.scheduleAtFixedRate({ update() }, 1, 10, TimeUnit.SECONDS)
+    }
+
     fun getCourseSummaries(studentId: Long): LiveData<List<CourseSummary>> {
-        val data = MutableLiveData<List<CourseSummary>>()
+        this.studentId = studentId
+        fetchCourseSummaries(studentId)
+        return dataCache
+    }
+
+    private fun fetchCourseSummaries(studentId: Long) {
         courseRemoteRepository
             .getCourseSummaries(studentId)
             .enqueue(object : Callback<List<CourseSummary>> {
                 override fun onResponse(call: Call<List<CourseSummary>>, response: Response<List<CourseSummary>>) {
-                    data.value = response.body()
+                    dataCache.value = response.body()
                 }
 
                 override fun onFailure(call: Call<List<CourseSummary>>, t: Throwable) {
-                    TODO()
+                    Log.e(javaClass.toString(), "Couldn't fetch dataCache from server")
                 }
             })
-        return data
+    }
+
+    private fun update() {
+        if (studentId != null)
+            fetchCourseSummaries(studentId!!)
     }
 }
